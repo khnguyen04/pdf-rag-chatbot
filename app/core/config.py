6009c7404953f423240559c0b09872247a66da49
Knguyennnnn <phanphamkhoanguyen031104@gmail.com>
2026-08-10 15:35:27 +0700

    chore: initialize PDF RAG chatbot project

---
+++ app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    app_name: str = "PDF RAG Chatbot"
+    app_version: str = "0.1.0"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from app.core.config import settings
+
+app = FastAPI(
+    title=settings.app_name,
+    version=settings.app_version,
+)
+
+@app.get("/")
+async def root():
+    return {
+        "message": "PDF RAG Chatbox is running"
+    }
